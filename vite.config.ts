99b8ad32b914dbf68c6c1abd20a6bf6460a5941f
paranoidkid (Max Zubko) <56440333+s1maxx@users.noreply.github.com>
2023-08-01 21:56:27 +0300

    fix homelink, indexes, env variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  base: "/myown_porfolio/"
+export default defineConfig(({command, mode})=> {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    define: {
+      'process.env': env
+    },
+    plugins: [react(), tsconfigPaths()],
+    base: env.VITE_HOME
+  }
 })
