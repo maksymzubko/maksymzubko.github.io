1ca3517539b0c96ad62d163722c47af6e15632c6
paranoidkid (Max Zubko) <56440333+s1maxx@users.noreply.github.com>
2023-08-01 18:08:17 +0300

    update and optimize models

--- src/components/canvas/ComputerCanvas.tsx
+++ src/components/canvas/ComputerCanvas.tsx
@@ -5,7 +5,7 @@ import {OrbitControls, Preload, useGLTF} from "@react-three/drei";
 import CanvasLoader from "@components/Loader.tsx";
 
 const Computer = ({isMobile}: {isMobile: boolean}) => {
-    const computer = useGLTF('./stylized_hacked_pc_sketchfabweeklychallenge/scene.gltf')
+    const computer = useGLTF(`./pc_${isMobile ? 'mobile' : 'desktop'}/scene.gltf`)
     return (
         <mesh>
             <hemisphereLight intensity={0.15} groundColor={"black"}/>
